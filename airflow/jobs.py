3ae6f2ec01d7538cbfd6dd2ae8da86797b080825
Benjamin Tallman <btallman@gmail.com>
2016-09-29 11:35:09 -0700

    Added logging to validate that our code is running

--- airflow/jobs.py
+++ airflow/jobs.py
@@ -679,6 +679,7 @@ class SchedulerJob(BaseJob):
         for a DAG based on scheduling interval
         Returns DagRun if one is scheduled. Otherwise returns None.
         """
+        self.logging.info('Creating DagRuns for {}'.format(dag.dag_id))
         if dag.schedule_interval:
             active_runs = DagRun.find(
                 dag_id=dag.dag_id,
@@ -723,6 +724,9 @@ class SchedulerJob(BaseJob):
                 self.logging.info('Backfill: {}.backfill = False, changing start_date to datetime.now()'.format(dag.dag_id))
                 if dag.start_date < datetime.now():
                     dag.start_date = datetime.now()
+            
+            else:
+                self.logging.info('Backfill: {}.backfill = True, not changing start_date to datetime.now()'.format(dag.dag_id))
 
             next_run_date = None
             if not last_scheduled_run:
